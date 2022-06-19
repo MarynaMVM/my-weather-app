317f8cef69926eab10158edc970f2b7dc7ee5c72
MarynaMVM <104380960+MarynaMVM@users.noreply.github.com>
2022-06-19 22:24:08 +0200

    Forecast added

--- src/app.js
+++ src/app.js
@@ -23,6 +23,51 @@ if (currentMinute < 10) {
 let finalDate = document.querySelector(".date");
 finalDate.innerHTML = `${currentDay} ${currentHour}:${currentMinute}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function searchLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
@@ -34,6 +79,14 @@ function searchLocation(position) {
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "57302bc3b033b0a28c10dc41ed39b53f";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
 }
 
 function showTemp(response) {
@@ -53,6 +106,8 @@ function showTemp(response) {
     "src",
     `http://openweathermap.org/img/wn/${mainIconURL}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
@@ -73,9 +128,4 @@ form.addEventListener("submit", handleSubmit);
 let currentLocBtn = document.querySelector("#secondary");
 currentLocBtn.addEventListener("click", getCurrentLocation);
 
-//function changeTempCels(event) {
-// event.preventDefault();
-//let temp = document.querySelector(".mainTemp");
-//temp.innerHTML = `29°`;
-//}
 searchCity("New York");
